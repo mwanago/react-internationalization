4d250f80c1d4a8a4e17950eaaf1b38b0ca4b3c4a
Marcin <wanago.marcin@gmail.com>
2024-05-12 23:56:07 +0200

    feat: translate the greeting

--- src/Greeting/index.tsx
+++ src/Greeting/index.tsx
@@ -1,3 +1,7 @@
+import { useTranslation } from 'react-i18next';
+
 export const Greeting = () => {
-  return <div>Hello</div>;
+  const { t } = useTranslation();
+
+  return <div>{t('greeting')}</div>;
 };
---
+++ src/i18next/index.tsx
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: {
+      greeting: 'Hello',
+    },
+  },
+  pl: {
+    translation: {
+      greeting: 'Cześć',
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export { i18n };
