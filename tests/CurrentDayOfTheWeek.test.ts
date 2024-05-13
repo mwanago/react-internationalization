81121b9e8c8f87d7959d8814e08e9827eecb7e88
Marcin <wanago.marcin@gmail.com>
2024-05-13 02:20:38 +0200

    test: test the translations with Playwright

--- tests/CurrentDayOfTheWeek.test.ts
+++ tests/CurrentDayOfTheWeek.test.ts
@@ -1,11 +1,31 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('The home page', () => {
-  test.describe('when visited', () => {
-    test('should contain the correct title', async ({ page }) => {
-      await page.goto('/');
-
-      await expect(page).toHaveTitle(/Vite/);
+test.describe('The current day of the week', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.addInitScript(() => {
+      const mockedDate = new Date('2024-05-14');
+      window.Date = function () {
+        return mockedDate;
+      };
+    });
+    await page.goto('/');
+  });
+  test.describe('when the locale is set to English', () => {
+    test.use({
+      locale: 'en',
+    });
+    test('should display in English', async ({ page }) => {
+      const text = page.getByText('Today is Tuesday.');
+      await expect(text).toBeVisible();
+    });
+  });
+  test.describe('when the locale is set to Polish', () => {
+    test.use({
+      locale: 'pl',
+    });
+    test('should display in Polish', async ({ page }) => {
+      const text = page.getByText('Dziś jest wtorek.');
+      await expect(text).toBeVisible();
     });
   });
 });
